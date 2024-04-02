210f351a0517d1db2d485e71a87e2b5295e54905
Harshvir Singh <118044782+HarshVir2003@users.noreply.github.com>
2024-04-02 22:05:02 +0530

    changes in urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("chat/", views.index, name="index"),
-    path("<str:room_name>/", views.room, name="room"),
+    path("chat/<str:room_name>/", views.room, name="room"),
 ]
